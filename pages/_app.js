c5b3d07f0c0ab4b7d5dec3f0ea9d357afd304119
yupimaru-sikaku <yukiharu.nagao@icloud.com>
2022-05-07 11:01:44 +0900

    概要構築

---
+++ pages/_app.js
@@ -0,0 +1,26 @@
+import '../styles/globals.css'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+/**
+ * The chain ID 4 represents the Rinkeby network
+ * チェーンID 4 は Rinkeby ネットワークを表します。
+ * The `injected` connector is a web3 connection method used by Metamask
+ * インジェクションコネクタは、Metamask が使用する web3 接続方法です。
+ */
+const supportedChainIds = [4]
+const connectors = {
+  injected: {},
+}
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
+}
+
+export default MyApp
---
+++ pages/nfts/[nftId].js
@@ -0,0 +1,94 @@
+import Header from '../../components/Header'
+import { useEffect, useMemo, useState } from 'react'
+import { useWeb3 } from '@3rdweb/hooks'
+import { ThirdwebSDK } from '@3rdweb/sdk'
+import { useRouter } from 'next/router'
+import NFTImage from '../../components/nft/NFTImage'
+import GeneralDetails from '../../components/nft/GeneralDetails'
+import ItemActivity from '../../components/nft/ItemActivity'
+import Purchase from '../../components/nft/Purchase'
+
+const style = {
+  wrapper: `flex flex-col items-center container-lg text-[#e5e8eb]`,
+  container: `container p-6`,
+  topContent: `flex`,
+  nftImgContainer: `flex-1 mr-4`,
+  detailsContainer: `flex-[2] ml-4`,
+}
+
+const Nft = () => {
+  const { provider } = useWeb3()
+  const [selectedNft, setSelectedNft] = useState()
+  const [listings, setListings] = useState([])
+  const router = useRouter()
+
+  const nftModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/INjSevyt3zKP9ReNYo-fen2IIpqvdgtJ'
+    )
+
+    return sdk.getNFTModule('0x6D866134Fc6367b910a1A362Efe8fa55DE94BB90')
+  }, [provider])
+
+  // get all NFTs in the collection
+  useEffect(() => {
+    if (!nftModule) return
+    ;(async () => {
+      const nfts = await nftModule.getAll()
+
+      const selectedNftItem = nfts.find((nft) => nft.id === router.query.nftId)
+
+      setSelectedNft(selectedNftItem)
+    })()
+  }, [nftModule])
+
+  const marketPlaceModule = useMemo(() => {
+    if (!provider) return
+
+    const sdk = new ThirdwebSDK(
+      provider.getSigner(),
+      'https://eth-rinkeby.alchemyapi.io/v2/INjSevyt3zKP9ReNYo-fen2IIpqvdgtJ'
+    )
+
+    return sdk.getMarketplaceModule(
+      '0x8b4ED7435cf55004eEEF66eF84b14b72429Fa5d4'
+    )
+  }, [provider])
+
+  useEffect(() => {
+    if (!marketPlaceModule) return
+    ;(async () => {
+      setListings(await marketPlaceModule.getAllListings())
+    })()
+  }, [marketPlaceModule])
+
+  return (
+    <div>
+      <Header />
+      <div className={style.wrapper}>
+        <div className={style.container}>
+          <div className={style.topContent}>
+            <div className={style.nftImgContainer}>
+              <NFTImage selectedNft={selectedNft} />
+            </div>
+            <div className={style.detailsContainer}>
+              <GeneralDetails selectedNft={selectedNft} />
+              <Purchase
+                isListed={router.query.isListed}
+                selectedNft={selectedNft}
+                listings={listings}
+                marketPlaceModule={marketPlaceModule}
+              />
+            </div>
+          </div>
+          <ItemActivity />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Nft
